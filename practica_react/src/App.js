a6e95aeea083316ef45bcf7ad421dd4c8e3eaff1
AncheJeez <andresjsangon@gmail.com>
2026-02-06 11:03:49 +0100

    añadido enrutador de react

--- practica_react/src/App.js
+++ practica_react/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Micomponente from './components/MiComponente';
@@ -8,26 +7,16 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <nav>
             <Link to="/">Home</Link> |{" "}
-            <Link to="/micomponente">Micomponente</Link> |{" "}
+            <Link to="/mibosque">MiBosque</Link> |{" "}
+            <Link to="/migatonaranja">MiGatoNaranja</Link> |{" "}
           </nav>
         </header>
         <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
-          <Route path="/micomponente" element={<Micomponente />} />
+          <Route path="/" element={<div>Esto es una prueba de home page</div>} />
+          <Route path="/mibosque" element={<Micomponente type="forest" />} />
+          <Route path="/migatonaranja" element={<Micomponente type="orange_cat" />} />
         </Routes>
       </div>
     </BrowserRouter>
--- practica_react/src/components/MiComponente.js
+++ practica_react/src/components/MiComponente.js
@@ -1,8 +1,26 @@
-function MiComponente() {
+import bosque from "../assets/forest.jpg";
+import gato_naranja from "../assets/orange-cat.jpeg";
+function MiComponente({type}) {
+
+  var imagen;
+
+  switch(type){
+    case "forest":
+      imagen = bosque;
+      break;
+    case "orange_cat":
+      imagen = gato_naranja;
+      break;
+    default:
+      imagen = bosque;
+  }
+
+
   return (
     <div>
       <h1>Mi Componente</h1>
       <p>Este es mi componente personalizado</p>
+      <img src={imagen} alt="Imagen" />
     </div>
   );
 }
